513a7f2193ca2c610c6b841d77d21b80cca417fd
Jonathan Cone <jonathancone@users.noreply.github.com>
2022-01-30 00:38:09 -0600

    Project reset

---
+++ pyleup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import sys
+
+if len(sys.argv) == 2:
+    print("1")
+else:
+    print("You need to supply a message for your project.")
+
+# git commit --date="10 day ago" -m "Your commit message"
